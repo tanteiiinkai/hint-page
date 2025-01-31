e9c94dec5d07ef35364e2e77ceb9e19ef2bfc150
blanket223 <xiaoq3427@gmail.com>
2025-01-31 21:07:23 +0900

    html inserted title tag, script inserted

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 const hint1 = document.querySelector('.i');
 const hint2 = document.querySelector('.ii');
-const hint3_1 = document.querySelector('.iii_i');
-const hint3_2 = document.querySelector('.iii_ii');
+const hint31 = document.querySelector('.iii_i');
+const hint32 = document.querySelector('.iii_ii');
 const bunshou1 = document.querySelector('.I');
 const bunshou2 = document.querySelector('.II');
-const bunshou3_1 = document.querySelector('.III_I');
-const bunshou3_2 = document.querySelector('.III_II');
+const bunshou31 = document.querySelector('.III_I');
+const bunshou32 = document.querySelector('.III_II');
+var hidecontent = "▼ヒントを隠す";
+var showcontent = "▶ヒントを表示する";
+function yobidashi(button, bunshou) {
+    if (button.textContent === hidecontent) {
+        destroy(button, bunshou); 
+    } else {
+        display(button, bunshou);
+    }
+
+}
 function display(button, bunshou) {
-    button.textContent = "▼ヒントを隠す";
+    button.textContent = hidecontent;
     console.log("おされた");
     bunshou.classList.add("show")
     bunshou.classList.remove("none");
 }
 function destroy(button, bunshou) {
-    button.textContent = "▶ヒントを表示する";
+    button.textContent = showcontent;
     console.log("閉じた");
     bunshou.classList.add("none");
     bunshou.classList.renive("show");
 }
-hint1.addEventListener('click', () => display(hint1, bunshou1));
-hint2.addEventListener('click', () => display(hint2, bunshou2));
-hint3_1.addEventListener('click', () => display(hint2, bunshou2));
-hint3_2.addEventListener('click', () => display(hint2, bunshou2));
+hint1.addEventListener('click', () => yobidashi(hint1, bunshou1));
+hint2.addEventListener('click', () => yobidashi(hint2, bunshou2));
+hint31.addEventListener('click', () => yobidashi(hint2, bunshou31));
+hint32.addEventListener('click', () => yobidashi(hint2, bunshou32));
